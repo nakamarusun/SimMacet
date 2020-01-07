d7b8bafe8e8eb7f01f5de4945622756d04e29de9
nakamarusun <48478366+nakamarusun@users.noreply.github.com>
2020-01-07 22:49:11 +0700

    Added math function for 2x2 determinant and check line intersections

--- Macet/math/custom_math_funcs.py
+++ Macet/math/custom_math_funcs.py
@@ -4,3 +4,20 @@ def vec2Projection(source: pygame.math.Vector2, dest: pygame.math.Vector2) -> py
     # Definition: Source vector is PROJECTED ONTO Dest vector
     divided = source.dot(dest) / dest.magnitude_squared()
     return pygame.math.Vector2([ divided * num for num in dest ])
+
+def determinant2x2(y1: list, y2: list):
+    return y1[0] * y2[1] - y1[1] * y2[0]
+
+def checkLineIntersection(Ax1y1: list, Ax2y2: list, Bx1y1: list, Bx2y2: list):
+    deltaX = (Ax1y1[0] - Ax2y2[0], Bx1y1[0] - Bx2y2[0])
+    deltaY = (Ax1y1[1] - Ax2y2[1], Bx1y1[1] - Bx2y2[1])
+
+    div = determinant2x2(deltaX, deltaY)
+    if div == 0:
+        return False, [0, 0]
+    
+    d = ( determinant2x2(Ax1y1, Ax2y2), determinant2x2(Bx1y1, Bx2y2) )
+    x = determinant2x2(d, deltaX) / div
+    y = determinant2x2(d, deltaY) / div
+
+    return True, [x, y]
