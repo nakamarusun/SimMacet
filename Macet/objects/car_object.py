changed car rotation anchor, positioning is still weird. road snap connection is fixed, now firstNode connects to snapped new node

Macet/objects/car_object.py
@@ -25,7 +25,7 @@ class Car(Object):
         self.nodeDestination: StreetNodes = nodeDest
         self.curSpeed = speed
         self.direction = 360 - (np.arctan2(*self.nodeAnchor.connectedNodes[self.nodeDestination][0][::-1]) * 180/math.pi)
-        self.image, rect = GMfun.rotationAnchor(self.originalImage, self.direction, [0, 0])
+        self.image, self.rect = GMfun.rotationAnchor(self.originalImage, self.direction, [0, 0])
 
     def update(self, coordsOffset):
         vector: pygame.math.Vector2 = self.nodeAnchor.connectedNodes[self.nodeDestination][0] # Define variables for vector from nodeAnchor to destination
@@ -48,11 +48,11 @@ class Car(Object):
         if change:
             vector: pygame.math.Vector2 = self.nodeAnchor.connectedNodes[self.nodeDestination][0]
             self.direction = 360 - (np.arctan2(*vector[::-1]) * 180/math.pi) # Fix this
-            self.image, rect = GMfun.rotationAnchor(self.originalImage, self.direction, [0, 0])
+            self.image, self.rect = GMfun.rotationAnchor(self.originalImage, self.direction, [0.5, 0.5])
         normalized = vector.normalize()
 
         self.speed = [ self.curSpeed * vec for vec in normalized ]
         super().update()
 
-        self.surface.blit(self.image, [ a - b for a, b in zip(self.coords, coordsOffset) ] )
+        self.surface.blit(self.image, [ a - b + c for a, b, c in zip(self.coords, coordsOffset, self.rect) ] )
         return False