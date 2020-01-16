d76e9fa7666d5f5556396fc5eda3e9f09b3eaa1d
nakamarusun <48478366+nakamarusun@users.noreply.github.com>
2020-01-16 16:47:01 +0700

    Initialized saver file

---
+++ Macet/saver.py
@@ -0,0 +1,8 @@
+# Script to store road data to Json file
+
+import json
+
+from objects.street_nodes import StreetNodes
+from objects.stop_light import StopLight
+from objects.car_spawner import CarSpawner
+from rooms.main_room import Canvas
